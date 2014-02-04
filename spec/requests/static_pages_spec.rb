ffa4b95c92db655c5c22f4b173e2abed1ec2a775
Alex Zhou <azhou@spokeo.com>
2014-02-04 11:33:49 -0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-
     it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -19,7 +26,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
@@ -32,12 +39,25 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
 
     it "should have the content 'About us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should ave the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
